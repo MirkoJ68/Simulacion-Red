b7adbcc383e037e570475bf0075a06e9a931a750
Miŕko con Pritty <d48669816@alumnos.itr.edu.ar>
2026-04-21 18:57:57 -0300

    primer commit

---
+++ Simulacion.py
@@ -0,0 +1,27 @@
+class Nodo:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.conexiones = []
+
+    def agregar_conexion(self, nodo):
+        self.conexiones.append(nodo)
+
+    def enviar_mensaje(self, mensaje):
+        print(f"{self.nombre} envía: {mensaje}")
+        for nodo in self.conexiones:
+            nodo.recibir_mensaje(mensaje, self.nombre)
+
+    def recibir_mensaje(self, mensaje, remitente):
+        print(f"{self.nombre} recibió de {remitente}: {mensaje}")
+
+
+servidor = Nodo("Servidor")
+cliente1 = Nodo("Cliente 1")
+cliente2 = Nodo("Cliente 2")
+cliente3 = Nodo("Cliente 3")
+
+servidor.agregar_conexion(cliente1)
+servidor.agregar_conexion(cliente2)
+servidor.agregar_conexion(cliente3)
+
+servidor.enviar_mensaje("Hola clientes, este es el servidor!")
